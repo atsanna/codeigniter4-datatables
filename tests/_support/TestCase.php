d6640721bc89571480a533b1131ab3caea29e15b
Antonio Sanna <atsanna@tiscali.it>
2022-06-08 12:27:54 +0200

    fix tests

--- tests/_support/TestCase.php
+++ tests/_support/TestCase.php
@@ -18,20 +18,5 @@ abstract class TestCase extends CIUnitTestCase
 
         parent::setUp();
 
-        // Use Array Settings Handler
-        $configSettings           = config('Settings');
-        $configSettings->handlers = ['array'];
-        $settings                 = new Settings($configSettings);
-        Services::injectMock('settings', $settings);
-
-        // Ensure from email is available anywhere during Tests
-        helper('setting');
-        setting('Email.fromEmail', 'foo@example.com');
-        setting('Email.fromName', 'John Smith');
-
-        // Clear any actions
-        $config          = config('Auth');
-        $config->actions = ['login' => null, 'register' => null];
-        Factories::injectMock('config', 'Auth', $config);
     }
 }
